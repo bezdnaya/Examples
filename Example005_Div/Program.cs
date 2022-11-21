a1115d6732c7cb60b312a1eb5a4f7055fe61922c
Irina <bezdnaya@mail.ru>
2022-11-21 20:08:47 +0200

    Добавили рандомный ввод числа

--- Example005_Div/Program.cs
+++ Example005_Div/Program.cs
@@ -1,5 +1,7 @@
-﻿double a = Convert.ToInt32(Console.ReadLine());
-double b = Convert.ToInt32(Console.ReadLine());
+﻿double a = new Random().Next(1,10);
+Console.WriteLine(a);
+double b = new Random().Next(1,10);
+Console.WriteLine(b);
 double result = a/b;
 Console.WriteLine(result);
 
