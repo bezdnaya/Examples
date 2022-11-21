f39eaf3091c84e38971a2fb0d65ac29cf44a8ed6
Irina <bezdnaya@mail.ru>
2022-11-21 19:43:12 +0200

    Example 003 Lucky name

---
+++ Example003_LuckyName/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя ");
+string name = Console.ReadLine();
+if (name.ToLower() == "ира")
+{
+    Console.WriteLine("Ура, это же Ира!");
+    }
+else {
+Console.Write ("Привет, ");
+Console.WriteLine (name);
+}
+
