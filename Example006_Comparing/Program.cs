d2b98aedf4633ea478e5cd06146609c73cf31da9
Irina <bezdnaya@mail.ru>
2022-11-21 20:24:15 +0200

    Example 006 Сравнение чисел

---
+++ Example006_Comparing/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 5;
+int b = 4;
+int c = 3;
+int d = 6;
+int e = 9;
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
