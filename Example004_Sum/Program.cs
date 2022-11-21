9e6324699429ee66a0c93ab4cc3b16e30f19c0f1
Irina <bezdnaya@mail.ru>
2022-11-21 18:50:09 +0200

    Example 004 Sum of 2 numbers

---
+++ Example004_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите 2 целых числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Их сумма ");
+Console.Write(a+b);
+
