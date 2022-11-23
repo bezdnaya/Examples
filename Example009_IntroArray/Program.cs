13e19e017d2b20e61127b98d951e51efdb665d58
Irina <bezdnaya@mail.ru>
2022-11-23 19:59:44 +0200

    Добавили заполнение массива рандомными числами

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -19,6 +19,26 @@ void PrintArray (int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int length = collection.Length;
+    int position = 0;
+    int index = -1;
+    while (position < length)
+    {
+        if (collection[position] == find)
+        {
+            index = position;
+            break;
+        }
+        position++;
+    }
+    return index;
+    }
+
 int [] array = new int[10];
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 9);
+Console.WriteLine(pos);
