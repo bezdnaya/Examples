3fd475445065f627de40c062ff91b160cab097b8
Irina <bezdnaya@mail.ru>
2022-11-23 16:44:50 +0200

    Сократили код

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -14,11 +14,7 @@ int b2 = 243;
 int c2 = 156;
 int a3 = 423;
 int b3 = 243;
-int c3 = 546;
+int c3 = 5946;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-
-int max = Max(max1, max2, max3);
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 Console.WriteLine("max = " + max);
