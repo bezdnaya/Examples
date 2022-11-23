be607be854780afef3655f81fa6300f4fd8298c6
Irina <bezdnaya@mail.ru>
2022-11-23 18:33:00 +0200

    Написали метод для заполнения массива

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -1,13 +1,24 @@
-﻿int[] array = {1,32,23,44,53,6,79,88,9};
-int n = array.Length;
-int find = 44;
-int index = 0;
-while (index < n)
+﻿void FillArray(int [] collection)
 {
-    if (array[index] == find)
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
     {
-        Console.WriteLine(index);
-        break;
+        collection[index] = new Random().Next (1,10);
+        index++;
     }
-    index++;
 }
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]+" ");
+        position++;
+    }
+}
+
+int [] array = new int[10];
+FillArray(array);
+PrintArray(array);
